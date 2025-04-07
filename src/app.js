0c5c1bfba2a192f3bc9afb69aa0160de45379c6c
Javiera Vega <javieravegareyes7@gmail.com>
2025-04-07 22:35:01 +0000

    Mi generador de cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let iconsArray = ["♦", "♥", "♠", "♣"]
+  let iconArraySelected = iconsArray[Math.floor(Math.random() * iconsArray.length)]
+  document.querySelector(".icons").innerHTML = iconArraySelected
+  document.querySelector(".iconBottom").innerHTML = iconArraySelected
+
+  let numberArray = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  document.querySelector("#number").innerHTML = numberArray[Math.floor(Math.random() * numberArray.length)]
+
+  if (iconArraySelected == "♦" || iconArraySelected == "♥") {
+    document.querySelector(".icons").style.color = "red";
+    document.querySelector(".iconBottom").style.color = "red";
+    document.querySelector("#number").style.color = "red";
+  }
+}
+
+
+
+
+
